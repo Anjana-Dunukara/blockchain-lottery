ba82b6462537c533549c3b787416fb230257397c
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-05-31 15:23:24 +0530

    initial commit

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "solar virus nothing clump reunion area abuse badge regular decide urge crater",
+  "https://sepolia.infura.io/v3/d9dab30341e64603baa3fc7bbeba814b"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attemptinf to deploy from acount ", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to ", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
