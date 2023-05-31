d9a61c51c2c4e6b7a5fd5fc9c791facc08c7a71f
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-05-31 20:33:52 +0530

    create test environment using ganache provider

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.4.17;
+pragma solidity ^0.8.17;
 
 contract Lottery{
     
     address public manager;
     address payable[] public players;
 
-    function Lottery() public{
+    constructor Lottery(){
         manager = msg.sender;
     }
 
