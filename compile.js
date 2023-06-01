e59dd2fb1f7360c88e9422c24ade7fd99bb797e9
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-06-01 13:42:44 +0530

    fix version incompatiblity

--- compile.js
+++ compile.js
@@ -5,4 +5,4 @@ const solc = require("solc");
 const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
 const source = fs.readFileSync(lotteryPath, "utf8");
 
-module.exports = solc.compile(source, 1).contracts[":Lottery"];
+console.log(solc.compile(source, 1).contracts[":Lottery"]);
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,37 +1,36 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.17;
+pragma solidity ^0.4.17;
 
-contract Lottery{
-    
+contract Lottery {
     address public manager;
-    address payable[] public players;
-
-    constructor Lottery(){
+    address[] public players;
+    
+    function Lottery() public {
         manager = msg.sender;
     }
-
+    
     function enter() public payable {
         require(msg.value > .01 ether);
-        players.push(payable(msg.sender));
+        players.push(msg.sender);
     }
-
+    
     function random() private view returns (uint) {
-       return  uint(keccak256(abi.encodePacked(block.difficulty , block.timestamp, players)));
+        return uint(keccak256(block.difficulty, now, players));
     }
-
-    function pickWinner() public restricted{
+    
+    function pickWinner() public restricted {
         uint index = random() % players.length;
-        players[index].transfer(address(this).balance);
-        players = new address payable[](0);
+        players[index].transfer(this.balance);
+        players = new address[](0);
     }
-
+    
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-
-    function getPlayers() public view returns (address payable[] memory){
+    
+    function getPlayers() public view returns (address[]) {
         return players;
     }
 }
