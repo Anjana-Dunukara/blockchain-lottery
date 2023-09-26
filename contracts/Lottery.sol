33f628c40c7c547310bf280a0aedb5ba6996ae28
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-09-26 10:52:30 +0530

    Add a variable to save the last winner

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -22,6 +22,7 @@ contract Lottery {
     function pickWinner() public restricted {
         uint index = random() % players.length;
         players[index].transfer(this.balance);
+        lastWinner = players[index];
         players = new address[](0);
     }
     
