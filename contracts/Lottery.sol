6ee53c40bcaba3dec5b50cbfb8442b529765e573
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-09-26 13:42:11 +0530

    Resolving solidity version bug

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -22,7 +22,7 @@ contract Lottery {
     function pickWinner() public restricted {
         uint index = random() % players.length;
         players[index].transfer(this.balance);
-        lastWinner = players[index];
+        address lastWinner = players[index];
         players = new address[](0);
     }
     
